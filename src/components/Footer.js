60591d49277814187b00a7ca82529d5cd8c8f2a1
DESKTOP-D59LRUN <caletina.marklester@gmail.com>
2023-02-07 03:02:00 +0800

    another one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center border-t my-2 '>
+    <div className='text-center border-t-2 my-2'>
         <p className='text-black font-Poppins py-2 '>Developed by Cherry Software 2023.</p>
     </div>
   )
